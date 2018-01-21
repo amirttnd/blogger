bda14e187d2288756e7cd150b01d6d3f927909f0
Mohd Amir <mohd.amir@tothenew.com>
2018-01-22 00:59:49 +0530

    display category on home page

--- src/main/java/in/tech/blogger/controller/BlogController.java
+++ src/main/java/in/tech/blogger/controller/BlogController.java
@@ -1,16 +1,29 @@
 package in.tech.blogger.controller;
 
+import in.tech.blogger.service.CategoryService;
+import in.tech.blogger.vo.CategoryVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class BlogController {
 
+    @Autowired
+    CategoryService categoryService;
+
     @RequestMapping({"/", "/blogs", "/blogs.html"})
-    String index() {
-        return "/blog/index";
+    ModelAndView index(@RequestParam(required = false) Long categoryId) {
+        ModelAndView modelAndView = new ModelAndView("/blog/index");
+        List<CategoryVO> categories = CategoryVO.toCategoryVO(categoryService.findAll());
+        modelAndView.addObject("categories", categories);
+        return modelAndView;
     }
 
 
---
+++ src/main/java/in/tech/blogger/vo/CategoryVO.java
@@ -0,0 +1,57 @@
+package in.tech.blogger.vo;
+
+
+import in.tech.blogger.domain.Category;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CategoryVO {
+
+    Long id;
+    String name;
+    Boolean active;
+
+    public CategoryVO(Category category) {
+        bind(category);
+    }
+
+    public void bind(Category category) {
+        this.id = category.getId();
+        this.name = category.getName();
+        this.active = category.getActive();
+    }
+
+    public static List<CategoryVO> toCategoryVO(List<Category> categories) {
+        if (categories != null) {
+            return categories.stream().map((category) -> {
+                return new CategoryVO(category);
+            }).collect(Collectors.toList());
+        }
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
