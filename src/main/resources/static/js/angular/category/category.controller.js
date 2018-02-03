96f92ccd4d7adf151fc55be4b121093e022d6860
Mohd Amir <mohd.amir@tothenew.com>
2018-02-03 16:17:01 +0530

    design tree

--- src/main/resources/static/js/angular/category/category.controller.js
+++ src/main/resources/static/js/angular/category/category.controller.js
@@ -27,18 +27,51 @@ angular
         Category.tree(function (response) {
             self.tree = response.tree;
             setTimeout(function () {
-                $("#category-tree").jstree(jsTreeConfig);
-                $compile($("#category-tree").html())($scope);
-                self.tree[0].subCategories.push({id:52,name:"dfadsf"})
+                var $categoryTree = $("#category-tree");
+                $categoryTree.jstree(jsTreeConfig);
+                var tree = angular.element($categoryTree.html());
+                $categoryTree.html(tree);
+                $compile(tree)($scope);
+                $scope.$digest()
 
             }, 0)
         });
 
         self.addChild = function (node) {
-            alert()
+            self.tree[0].subCategories=[]
             if (angular.isDefined(node) && node.subCategories) {
-                node.subCategories.push({})
+                node.subCategories.push({id: "Dafda", name: "dfdsafsdfsda"})
             }
         }
 
     }]);
+
+var AngularHelper = (function () {
+    var AngularHelper = function () {
+    };
+
+    /**
+     * ApplicationName : Default application name for the helper
+     */
+    var defaultApplicationName = "techBlogger";
+
+    /**
+     * Compile : Compile html with the rootScope of an application
+     *  and replace the content of a target element with the compiled html
+     * @$targetDom : The dom in which the compiled html should be placed
+     * @htmlToCompile : The html to compile using angular
+     * @applicationName : (Optionnal) The name of the application (use the default one if empty)
+     */
+    AngularHelper.Compile = function ($targetDom, htmlToCompile, applicationName) {
+        var $injector = angular.injector(["ng", applicationName || defaultApplicationName]);
+
+        $injector.invoke(["$compile", "$rootScope", function ($compile, $rootScope) {
+            //Get the scope of the target, use the rootScope if it does not exists
+            var $scope = $targetDom.html(htmlToCompile).scope();
+            $compile($targetDom)($scope || $rootScope);
+            $rootScope.$digest();
+        }]);
+    }
+
+    return AngularHelper;
+})();
