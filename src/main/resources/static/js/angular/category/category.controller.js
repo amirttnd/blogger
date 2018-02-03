850f09b7d5a900d2aae13f0f82ce6b83d88e4557
Mohd Amir <mohd.amir@tothenew.com>
2018-02-03 15:03:16 +0530

    imported

--- src/main/resources/static/js/angular/category/category.controller.js
+++ src/main/resources/static/js/angular/category/category.controller.js
@@ -1,32 +1,44 @@
 angular
     .module("techBlogger")
-    .controller("CategoryController", ["Category", function (Category) {
+    .controller("CategoryController", ["Category", "$compile", "$scope", function (Category, $compile, $scope) {
         var self = this;
+
+        var jsTreeConfig = {
+            "types": {
+                "default": {
+                    "icon": "fa fa-flash"
+                }
+            },
+            "core": { // core options go here
+                "multiple": true, // no multiselection
+                "check_callback": true,
+                "themes": {
+                    "dots": true, // no connecting dots between dots
+                    "variant": "large"
+
+                },
+                "state": {
+                    opened: true
+                }
+            },
+            "plugins": ["state", "types"] // activate the state plugin on this instance
+        };
 
         Category.tree(function (response) {
             self.tree = response.tree;
             setTimeout(function () {
-                $("#category-tree").jstree({
-                    "types": {
-                        "default": {
-                            "icon": "fa fa-flash"
-                        }
-                    },
-                    "core": { // core options go here
-                        "multiple": true, // no multiselection
-                        "check_callback": true,
-                        "themes": {
-                            "dots": true, // no connecting dots between dots
-                            "variant": "large"
+                $("#category-tree").jstree(jsTreeConfig);
+                $compile($("#category-tree").html())($scope);
+                self.tree[0].subCategories.push({id:52,name:"dfadsf"})
 
-                        },
-                        "state": {
-                            opened: true
-                        }
-                    },
-                    "plugins": ["state", "types"] // activate the state plugin on this instance
-                });
             }, 0)
         });
+
+        self.addChild = function (node) {
+            alert()
+            if (angular.isDefined(node) && node.subCategories) {
+                node.subCategories.push({})
+            }
+        }
 
     }]);
