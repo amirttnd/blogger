748b9bb28834b530e0c0ca937d542a3081903c01
Mohd Amir <mohd.amir@tothenew.com>
2018-02-04 13:38:33 +0530

    added filter.js

--- src/main/resources/static/js/angular/blog/blog.controller.js
+++ src/main/resources/static/js/angular/blog/blog.controller.js
@@ -1,10 +1,25 @@
 angular
     .module("techBlogger")
     .controller("BlogController", ["Blog", "Category", "$scope", function (Blog, Category, $scope) {
-        var self = this
+        var self = this;
+
+        self.tags = [];
+
+        self.relatedCategories = [];
+
+        self.mainCategory = {};
+
 
         Category.tree(function (response) {
             self.tree = response.tree;
             console.log(self.tree)
-        })
+        });
+
+        self.setMainCategory = function (category) {
+            self.mainCategory = category;
+        };
+
+        self.addToRelatedCategory = function (category) {
+            self.relatedCategories.push(category);
+        }
     }]);
