97b7674b4b7a12355e17d1ae52bab6cdd228d999
Mohd Amir <mohd.amir@tothenew.com>
2018-02-10 15:19:39 +0530

    Improved UI of menu

--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -1,13 +1,4 @@
 $(document).ready(function () {
-    CKEDITOR.editorConfig = function (config) {
-        config.language = 'es';
-        config.height = 100;
-        config.toolbarCanCollapse = true;
-        config.uiColor = '#9AB8F3'
-
-
-    };
-    CKEDITOR.replaceAll("ckTextEditor");
-
+    $('html,body').animate({scrollTop: 65}, 1000);
 
 });
