0a8ff4dca85fd013aea42505a4697bcefbacc921
Mohd Amir <mohd.amir@tothenew.com>
2018-02-12 02:03:03 +0530

    changed menu design

--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
     //$('html,body').animate({scrollTop: 65}, 1000);
-
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 135) {
+            $("#navigation").addClass("fixed")
+        } else {
+            $("#navigation").removeClass("fixed")
+        }
+    })
 });
